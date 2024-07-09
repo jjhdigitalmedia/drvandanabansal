6001a76f39109adb49e540cf7f11316cb9d37143
salman ansari <salmanansari1409@gmail.com>
2024-07-09 12:53:57 +0530

    last

--- src/Component/NavigationB.jsx
+++ src/Component/NavigationB.jsx
@@ -94,11 +94,14 @@ function NavigationB() {
                     </Menu>
                 </div>
                 <div>
-                    <a href='bookappointment'>
-                    <button className="rounded-lg inline-flex items-center bg-blue-700 border-0 py-2 px-4 text-white">
+                    {/* <Link to='bookappointment'> */}
+                    {/* <button className="rounded-lg inline-flex items-center bg-blue-700 border-0 py-2 px-4 text-white"> */}
+                        {/* <span className='className="rounded-lg inline-flex items-center bg-blue-700 border-0 py-2 px-4 text-white"'></span> */}
+                        <a  href="bookappointment" className='className="rounded-lg inline-flex items-center bg-blue-700 border-0 py-2 px-4 text-white'>
                         Book Appointment
-                    </button>
-                    </a>
+                        </a>
+                    {/* </button> */}
+                    {/* </Link> */}
                 </div>
             </div>
         </nav>
