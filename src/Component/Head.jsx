85702f98ca1a3325f5ca94a161205594d0a6d445
salman ansari <salmanansari1409@gmail.com>
2024-07-11 11:12:43 +0530

    a

--- src/Component/Head.jsx
+++ src/Component/Head.jsx
@@ -7,10 +7,11 @@ import '../Style/Style.css'
 const Head = () => {
   return (
     <>
-      <section class="text-gray-600 body-font bg-[url('https://i.postimg.cc/15W218N9/bgvandana1.png')] bg-hero bg-no-repeat bg-cover bg-center bg-fixed">
+    {/* https://i.postimg.cc/15W218N9/bgvandana1.png */}
+      <section class="text-gray-600 body-font bg-[url(https://i.postimg.cc/52gCJzf7/nn.jpg)] bg-hero g-no-repeat bg-cover bg-center bg-fixed" style ={ { backgroundImage: "https://i.postimg.cc/52gCJzf7/nn.jpg')" } }>
         <div class="container mx-auto w-11/12 flex px-2 pt-28 pb-36 md:pt-32 md:pb-16 lg:pt-40 lg:w-4/5 md:flex-row text-center flex-col items-center">
           <div class="pr-0 w-11/12 m-auto text-center flex flex-col md:w/2 md:pr-8 lg-pr-8 md:items-start lg:flex-grow lg:pl-4">
-            <h1 class="title-font sm:text-4xl md:text-left text-3xl mb-3 text-pink-700 font-bold [text-shadow:_0_2px_0_rgb(0_0_0_/_10%)]">
+            <h1 class="title-font sm:text-4xl md:text-left text-3xl mb-3 text-rose-600 font-bold [text-shadow:_0_2px_0_rgb(0_0_0_/_10%)]">
               Best Gynaecologist in Allahabad
             </h1>
             <span className="font-bold mb-2 text-white text-xl md: text-left">Retired Chief Medical Superintendent
@@ -20,12 +21,12 @@ const Head = () => {
               and outpatients, manage hospital administration, and establish protocols for
               comprehensive patient care.
             </p>
-            <span className="font-bold text-xl text-pink-700 my-3">40 years of experience</span>
+            <span className="font-bold text-xl text-rose-600 my-3">40 years of experience</span>
             <div className="mb-3">
               <h1 className="font-bold text-xl">Medical Council Registration </h1>
-              <p>UP State Medical Council Registration No: <span className="text-pink-800">24503</span>  </p>
+              <p>UP State Medical Council Registration No: <span className="text-rose-600">24503</span>  </p>
             </div>
-            <Link to='bookappointment' className="rounded-lg inline-flex items-center bg-pink-700 border-0 py-2 px-4 text-white">
+            <Link to='bookappointment' className="rounded-lg inline-flex items-center bg-rose-600 border-0 py-2 px-4 text-white">
               <button>
                 book Appointment
               </button>
