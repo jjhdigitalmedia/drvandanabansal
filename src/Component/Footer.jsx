0119eb2a8ac94fc774c94394a03e033b9bca8002
salman ansari <salmanansari1409@gmail.com>
2024-07-31 17:44:48 +0530

    final

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -24,19 +24,19 @@ const Footer = () => {
               </nav>
             </div>
             <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+              <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">Important Links</h2>
               <nav class="list-none mb-10">
                 <li>
-                  <a class="text-gray-600  hover:text-gray-800">First Link</a>
+                  <a class="text-gray-600  hover:text-gray-800">Home</a>
                 </li>
                 <li>
-                  <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+                  <a class="text-gray-600 hover:text-gray-800">About</a>
                 </li>
                 <li>
-                  <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+                  <a class="text-gray-600 hover:text-gray-800">Achievements</a>
                 </li>
                 <li>
-                  <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                  <a class="text-gray-600 hover:text-gray-800">Contacts</a>
                 </li>
               </nav>
             </div>
@@ -69,7 +69,7 @@ const Footer = () => {
         <div class="bg-slate-100">
           <div class="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
             <a class="text-rose-700 flex title-font font-medium items-center md:justify-start justify-center">
-              <span class="ml-3 text-xl">Best Gynaecologist in Prayagraj</span>
+              <span class="ml-3 text-xl">Best IVF Expert in Prayagraj</span>
             </a>
             <p class="text-sm text-gray-500 sm:ml-6 sm:mt-0">© 2024 All rights reserved by Dr. Vandana Bansal
             </p>
@@ -85,6 +85,9 @@ const Footer = () => {
                   <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                 </svg>
               </a>
+              
+              <FontAwesomeIcon icon="fa-brands fa-youtube" />
+
               <a href='https://www.instagram.com/drvandanabansal_gynaecologist/' target='_blank' class="text-rose-700 ml-3 ">
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                   <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
--- src/Component/NavigationB.jsx
+++ src/Component/NavigationB.jsx
@@ -51,7 +51,7 @@ function NavigationB() {
                         Contact
                     </a>
 
-                    <Menu as="div" className="relative inline-block text-left">
+                    {/* <Menu as="div" className="relative inline-block text-left">
                         <div>
                             <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-transparent px-3 py-2 text-sm font-semibold border-sm border-pink-800 ring-1 ring-inset ring-white hover:bg-gray-50 md:text-white text-black">
                                 Options
@@ -90,7 +90,7 @@ function NavigationB() {
                                 </MenuItem>
                             </div>
                         </MenuItems>
-                    </Menu>
+                    </Menu> */}
                 </div>
 
                 <button className="rounded-lg inline-flex items-center bg-rose-700 border-0 py-2 px-4 text-white">
