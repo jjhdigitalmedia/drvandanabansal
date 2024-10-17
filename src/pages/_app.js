db0436ac7c2c34226bfb8643f6ee707c9ea9dc0c
unknown <salmanansari1409@gmail.com>
2024-10-17 17:07:51 +0530

    ff

---
+++ src/pages/_app.js
@@ -0,0 +1,9 @@
+import "@/styles/globals.css";
+import Home from ".";
+
+export default function App({ Component, pageProps }) {
+  return <>
+    {/* <Home />*/}
+    <Component {...pageProps} />
+  </>
+}
