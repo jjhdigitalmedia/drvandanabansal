a7e3644414232aa5ac6bc8cb346ec713a4d2599b
unknown <jjhdigitalmedia@gmail.com>
2025-02-26 16:38:15 +0530

    ff

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -9,7 +9,7 @@ import gallery06 from '../assets/Gallery/gallery06.jpg';
 import gallery09 from '../assets/Gallery/gallery09.png';
 import gallery10 from '../assets/Gallery/gallery10.png';
 import gallery11 from '../assets/Gallery/gallery11.png';
-import A01w from '../assets/Gallery/A01w.jpg';
+import A01a from '../assets/Gallery/A01a.jpg';
 import B02 from '../assets/Gallery/B02.jpg';
 import PageBanner from './PageBanner';
 
@@ -38,7 +38,7 @@ const Gallery = () => {
                     </div>
                     <div class="flex flex-wrap w-1/2">
                         <div class="md:p-2 p-1 w-full">
-                            <img alt="gallery" class="w-full h-full object-cover object-center block" src={A01w} />
+                            <img alt="gallery" class="w-full h-full object-cover object-center block" src={A01a} />
                         </div>
                         <div class="md:p-2 p-1 w-1/2">
                             <img alt="gallery" class="w-full object-cover h-full object-center block" src={gallery05} />
