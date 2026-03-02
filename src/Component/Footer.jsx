3b9954e48cbe39ecefc34e03a8848fbd25f040ed
salman <jjhdigitalmedia@gmail.com>
2026-03-02 17:18:08 +0530

    ff

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
                 <Link class="flex hover:text-white items-center hover:text-primary gap-1 hover:ml-1 duration-200" to="/treatments/iui">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right">
                     <path d="M5 12h14"></path><path d="m12 5 7 7-7 7"></path></svg>
-                  <IoIosArrowForward className="inline" />
+                  {/* <IoIosArrowForward className="inline" /> */}
                   <span className="">IUI</span>
                 </Link>
               </li>
